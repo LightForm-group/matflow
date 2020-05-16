a88a14ca3c298dcf67b1392333864830f68033ee
aplowman <adam.jp@live.co.uk>
2020-05-16 14:54:02 +0100

    Avoid import error!

--- matflow/__init__.py
+++ matflow/__init__.py
@@ -1,13 +1,6 @@
 """`matflow.__init__.py`"""
 
 from matflow._version import __version__
-from matflow.api import (
-    make_workflow,
-    submit_workflow,
-    load_workflow,
-    append_schema_source,
-    prepend_schema_source,
-)
 
 # These dicts map task/method/implementations to specific Python functions.
 TASK_INPUT_MAP = {}
@@ -18,3 +11,11 @@ TASK_OUTPUT_FILES_MAP = {}
 SOFTWARE_VERSIONS = {}
 EXTENSIONS = {}
 SCHEMA_IS_VALID = {}
+
+from matflow.api import (
+    make_workflow,
+    submit_workflow,
+    load_workflow,
+    append_schema_source,
+    prepend_schema_source,
+)
