b5f4b0dbe4726ddf9a7ee6a687e86b3c2855b4e7
APlowman <adam.jp@live.co.uk>
2020-12-16 15:36:21 +0000

    Bump hpcflow to v0.1.12

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     },
     install_requires=[
         'matflow-demo-extension',
-        'hpcflow>=0.1.10',
+        'hpcflow>=0.1.12',
         'click>7.0',
         'hickle>=4.0.1',
         'ruamel.yaml',
